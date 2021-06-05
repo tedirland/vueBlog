5689ed3bd1e7d98fa1894b60e86af6399d15f7e5
Ted Irland <irlandth@gmail.com>
2021-06-05 10:18:02 -0500

    Created new project w/ boilerplate code from cli

---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+<h1>Home
+  <p>My name is {{name}} and my age is {{age}}</p>
+  <button @click="handleClick">Click meh</button>
+
+</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  setup() {
+  let name = 'mario'
+  let age = 30
+
+  const handleClick = () => {
+    console.log('you clicked meh bruh')
+  }
+
+  return {name, age, handleClick}
+  },
+
+
+  }
+
+</script>
