f7d34fd51dad759d483862df51579fe3b38f06c2
Ted Irland <irlandth@gmail.com>
2021-06-05 11:00:23 -0500

    Used the computed property inside the setup hook to filter an array based on an input value. Created an input with the v-model directive and then used a v-for directive to find name (passed into the names.value.filter method) to create a computed value of matchingNames

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
 <h1>Home</h1>
-<h2>Refs</h2>
-<p>{{ninjaOne.name}} - {{ninjaOne.age}}</p>
-<button @click="updateNinjaOne">Update Ninja One</button>
-<h2>Reactive</h2>
-<p>{{ninjaTwo.name}} {{ninjaTwo.age}} - {{nameTwo}}</p>
-<button @click="updateNinjaTwo">Update Ninja Two</button>
+<input type="text" v-model="search">
+<p>Search Term: {{search}}</p>
+<div v-for="name in matchingNames" :key="name">{{name}}</div>
+
   </div>
 </template>
 
 <script>
 import { ref, reactive } from '@vue/reactivity'
-import { vModelRadio } from '@vue/runtime-dom'
+import { computed } from '@vue/runtime-dom'
 // @ is an alias to /src
 
 
@@ -20,22 +18,14 @@ export default {
   name: 'Home',
   setup() {
 
- const ninjaOne = ref({name: 'mario', age:30})
- //Drawbacks to using reactive:
- //Can't do this: (can't use primitive values w/ reactives)
- const nameTwo = reactive('luigi')
+  const search = ref('')
+  const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
 
- const nameOne = ref('mario')
- const ninjaTwo = reactive({name: 'luigi', age: 35})
+  const matchingNames = computed(() => {
+    return names.value.filter((name) => name.includes(search.value))
+  })
 
- const updateNinjaOne = () => {
-   ninjaOne.value.age = 40
- }
- const updateNinjaTwo = () => {
-   ninjaTwo.age=45
-   nameTwo = "mario"
- }
- return {ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo, nameOne, nameTwo}
+ return {names,search,matchingNames }
   },
 
 
