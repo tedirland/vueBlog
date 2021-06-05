866b9ccf2024b92a9d8570a5537d0873825d1972
Ted Irland <irlandth@gmail.com>
2021-06-05 12:18:00 -0500

    Played around with lifecycle hooks inside the setup function. The only major difference is they are all called on<hookname>and must be imported from the vue library

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
 <h1>Home</h1>
-<PostList :posts="posts" />
+<PostList v-if="showPosts" :posts="posts" />
+<button @click="showPosts = !showPosts">toggle posts</button>
+<button @click="posts.pop()">delete a post</button>
 <p></p>
   </div>
 </template>
@@ -22,7 +24,11 @@ export default {
 
  ])
 
- return {posts }
+
+
+ const showPosts = ref(true)
+
+ return {posts, showPosts }
   },
 
 
