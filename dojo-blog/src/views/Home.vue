0f38b46d0624dca456ac1369b09107ecc26d868f
Ted Irland <irlandth@gmail.com>
2021-06-05 11:10:07 -0500

    Played around with watch and watchEffect. Both are used to keep an eye on a specific value and run a function when it changes. Most common use case is to fetch data from an api

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -4,13 +4,14 @@
 <input type="text" v-model="search">
 <p>Search Term: {{search}}</p>
 <div v-for="name in matchingNames" :key="name">{{name}}</div>
+<button @click="handleClick">Stop Watching</button>
 
   </div>
 </template>
 
 <script>
 import { ref, reactive } from '@vue/reactivity'
-import { computed } from '@vue/runtime-dom'
+import { computed, watch, watchEffect } from '@vue/runtime-dom'
 // @ is an alias to /src
 
 
@@ -21,11 +22,27 @@ export default {
   const search = ref('')
   const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
 
+  const stopWatch = watch(search, () => {
+    console.log('watch function run')
+  })
+
+//Most common usecase is to get data from a database
+  const stopEffect = watchEffect(() => {
+    console.log('watchEffect function ran', search.value)
+  })
+
+  const handleClick = () => {
+    stopWatch()
+    stopEffect()
+
+
+  }
+
   const matchingNames = computed(() => {
     return names.value.filter((name) => name.includes(search.value))
   })
 
- return {names,search,matchingNames }
+ return {names,search,matchingNames, handleClick }
   },
 
 
