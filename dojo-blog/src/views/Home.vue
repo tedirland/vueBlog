db7c3f1e8b71d1744c8ad49c741e067c9e736999
Ted Irland <irlandth@gmail.com>
2021-06-05 10:27:31 -0500

    Added some dom elements to experement with the setup function inside the scripts section of the component creating a ref can be used to targed and manipulate a specific dom element before anything is rendered to the screen. Cannot use this since it runs before the component is created

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
 <h1>Home
-  <p>My name is {{name}} and my age is {{age}}</p>
+  <p ref="p">My name is {{name}} and my age is {{age}}</p>
   <button @click="handleClick">Click meh</button>
 
 </h1>
@@ -9,20 +9,26 @@
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 // @ is an alias to /src
 
 
 export default {
   name: 'Home',
   setup() {
+
+  const p  = ref('hello')
+  console.log(p, p.value)
   let name = 'mario'
   let age = 30
 
   const handleClick = () => {
-    console.log('you clicked meh bruh')
+    console.log(p, p.value)
+    p.value.classList.add('test')
+    p.value.textContent = 'hello ninjas'
   }
 
-  return {name, age, handleClick}
+  return {name, age, handleClick, p}
   },
 
 
