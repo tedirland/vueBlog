d7a7cf55cb40fe5d7a449d1bab44fdcbc4eaff2a
Ted Irland <irlandth@gmail.com>
2021-06-06 16:59:22 -0500

    Created a tag route that will fetch all posts by a specfic tag, initiated by clicking on a certian tag

---
+++ dojo-blog/src/views/Tag.vue
@@ -0,0 +1,28 @@
+<template>
+ <div class="home">
+<h1>Tags</h1>
+<div v-if="error">{{error}}</div>
+<div v-if="posts.length"><PostList :posts="posts"  /></div>
+<div v-else><Spinner /></div>
+ </div>
+</template>
+
+<script>
+import getPosts from '../composables/getPosts'
+import PostList from '../components/PostList.vue'
+export default {
+  components: {PostList},
+
+  setup() {
+    const {posts, error, load} = getPosts()
+    load() //I assume we pass the tag here
+
+    return {posts, error}
+  }
+
+}
+</script>
+
+<style>
+
+</style>
