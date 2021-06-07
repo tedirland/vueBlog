0047f45f0d3369f6ca3f7f587a8ba878dc76ba6b
Ted Irland <irlandth@gmail.com>
2021-06-07 10:27:04 -0500

    Replaced the create path with the collection.add method and passed in the post object we create from modeling the form in Create.vue

--- dojo-blog/src/views/Create.vue
+++ dojo-blog/src/views/Create.vue
@@ -21,6 +21,7 @@
 <script>
 import {ref} from 'vue'
 import {useRouter, useRoute} from 'vue-router'
+import { projectFirestore } from '../firebase/config'
 export default {
   setup() {
     const title = ref('')
@@ -44,11 +45,8 @@ export default {
         body: body.value,
         tags: tags.value,
       }
-      await fetch(' http://localhost:3000/posts', {
-        method: 'POST',
-        headers: {'Content-Type' : 'application/json'},
-        body: JSON.stringify(post)
-      })
+     const res = await projectFirestore.collection('posts').add(post)
+
       router.push({name: 'Home'})
     }
 
